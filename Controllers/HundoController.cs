14ade21db90dde6244da82bf97a1ca51cf4c25b7
BelleCode <Belinda.tiberio@gmail.com>
2021-10-11 15:10:53 -0700

    Add project files.

---
+++ Controllers/HundoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCHundo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCHundo.Controllers
+{
+    public class HundoController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(HundoNumbers nums)
+        {
+            //generate a list of functions with results properties
+            List<string> numItems = new List<string>();
+
+            for (int i = nums.StartValue; i <= nums.EndValue; i++)
+            {
+                numItems.Add(i.ToString());
+            }
+
+            nums.Results = numItems;
+
+            return View(nums);
+        }
+
+        public IActionResult Index()
+        {
+            HundoNumbers nums = new HundoNumbers();
+
+            nums.StartValue = 1;
+            nums.EndValue = 100;
+
+            return View(nums);
+        }
+    }
+}
---
+++ Models/HundoNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCHundo.Models
+{
+    public class HundoNumbers
+    {
+        public int StartValue { get; set; }
+        public int EndValue { get; set; }
+
+        //lists cannot be NULL
+        public List<string> Results { get; set; } = new List<string>();
+    }
+}
